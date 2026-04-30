011752749dc8631bd928bcffd1b1e01bcdeba4b0
unitdevgcc <176902683+unitdevgcc@users.noreply.github.com>
2026-04-30 20:18:04 +0300

    ьу

--- server/src/test/java/dev/c0redev/volter/RelayRegistryTest.java
+++ server/src/test/java/dev/c0redev/volter/RelayRegistryTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RelayRegistryTest {
   @Test
   void enforcesPerRemoteLimit() {
-    RelayRegistry r = new RelayRegistry();
+    RelayRegistry r = new RelayRegistry(64, 2048);
     int ok = 0;
     for (int i = 0; i < 70; i++) {
       if (r.tryAcquire("a")) ok++;
